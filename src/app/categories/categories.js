54b3bddacd5862d32dd95adc7cf8862d59c61d48
Joel Hooks <joelhooks@gmail.com>
2014-11-03 09:42:16 -0600

    add named views

--- src/app/categories/categories.js
+++ src/app/categories/categories.js
@@ -1,3 +1,26 @@
 angular.module('categories', [
     'eggly.models.categories'
-]);
+])
+    .config(function($stateProvider){
+        $stateProvider
+            .state('eggly.categories', {
+                url: '/',
+                views: {
+                    'categories@': {
+                        controller: 'CategoriesCtrl',
+                        templateUrl: 'app/categories/categories.tmpl.html'
+                    },
+                    'bookmarks@': {
+                        controller: 'BookmarksCtrl',
+                        templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html'
+                    }
+                }
+            })
+    })
+    .controller('CategoriesCtrl', function CategoriesCtrl($scope){
+
+    })
+    .controller('BookmarksCtrl', function BookmarksCtrl($scope) {
+
+    })
+;
--- src/app/eggly-app.finish.js
+++ src/app/eggly-app.finish.js
@@ -3,14 +3,15 @@ angular.module('Eggly', [
     'categories',
     'categories.bookmarks'
 ])
-    .config(function ($stateProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('eggly', {
-                url: '/',
-                templateUrl: 'app/categories/categories.tmpl.html',
-                controller: 'MainCtrl'
+                url: '',
+                abstract: true
             })
         ;
+
+        $urlRouterProvider.otherwise('/');
     })
     .controller('MainCtrl', function ($scope) {
         $scope.categories = [
@@ -21,15 +22,15 @@ angular.module('Eggly', [
         ];
 
         $scope.bookmarks = [
-            {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
-            {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
-            {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
-            {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
-            {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
-            {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
-            {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
-            {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
-            {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+            {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development"},
+            {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development"},
+            {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design"},
+            {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design"},
+            {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise"},
+            {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise"},
+            {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor"},
+            {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor"},
+            {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor"}
         ];
 
         $scope.isCreating = false;
