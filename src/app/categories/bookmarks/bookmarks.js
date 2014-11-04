6db9f479e0436dc446d981828e732dac1ff6286e
Lukas Ruebbelke <simpul@gmail.com>
2014-11-04 10:12:56 -0700

    Adding in state params

--- src/app/categories/bookmarks/bookmarks.js
+++ src/app/categories/bookmarks/bookmarks.js
@@ -3,4 +3,21 @@ angular.module('categories.bookmarks', [
     'categories.bookmarks.edit',
     'eggly.models.categories',
     'eggly.models.bookmarks'
-]);
+])
+    .config(function($stateProvider){
+        $stateProvider
+            .state('eggly.categories.bookmarks', {
+                url: 'categories/:category',
+                views: {
+                    'bookmarks@': {
+                        templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html',
+                        controller: 'BookmarksCtrl'
+                    }
+                }
+            })
+        ;
+    })
+    .controller('BookmarksCtrl', function ($scope,$stateParams  ) {
+        $scope.currentCategoryName = $stateParams.category;
+    })
+;
