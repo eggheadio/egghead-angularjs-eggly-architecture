143cd034c6f893cd972b8d7242029bf025496e3d
Lukas Ruebbelke <simpul@gmail.com>
2014-11-25 22:40:41 -0700

    Clean branch for basic state lesson.

--- src/app/eggly-app.js
+++ src/app/eggly-app.js
@@ -1,7 +1,17 @@
 angular.module('Eggly', [
+    'ui.router',
     'categories',
     'categories.bookmarks'
 ])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('eggly', {
+                url: '/', // Make to navigate to index.html#/
+                templateUrl: 'app/categories/categories.tmpl.html',
+                controller: 'MainCtrl'
+            })
+        ;
+    })
     .controller('MainCtrl', function ($scope) {
         $scope.categories = [
             {"id": 0, "name": "Development"},
