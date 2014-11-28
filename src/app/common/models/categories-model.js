11f5e3642342d67e0a7a82ad4b046d56598d650a
Lukas Ruebbelke <simpul@gmail.com>
2014-11-28 00:48:16 -0700

    Finished promises lesson code.

--- src/app/common/models/categories-model.js
+++ src/app/common/models/categories-model.js
@@ -1,7 +1,7 @@
 angular.module('eggly.models.categories', [
 
 ])
-    .service('CategoriesModel', function ($http) {
+    .service('CategoriesModel', function ($http, $q) {
         var model = this,
             URLS = {
                 FETCH: 'data/categories.json'
@@ -18,7 +18,30 @@ angular.module('eggly.models.categories', [
         }
 
         model.getCategories = function() {
-            return $http.get(URLS.FETCH).then(cacheCategories);
-        }
+            return (categories) ? $q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
+        };
+
+        model.getCategoryByName = function(categoryName) {
+            var deferred = $q.defer();
+
+            function findCategory(){
+                return _.find(categories, function(c){
+                    return c.name == categoryName;
+                })
+            }
+
+            if(categories) {
+                deferred.resolve(findCategory());
+            } else {
+                model.getCategories()
+                    .then(function() {
+                        deferred.resolve(findCategory());
+                    });
+            }
+
+            return deferred.promise;
+        };
+
+
     })
 ;
