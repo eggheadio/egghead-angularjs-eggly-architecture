15648243c5563165acd3e45e811a8744de24f4dd
Lukas Ruebbelke <simpul@gmail.com>
2014-12-06 13:39:27 -0700

    Fixing broken route

--- src/app/categories/categories.js
+++ src/app/categories/categories.js
@@ -9,6 +9,10 @@ angular.module('categories', [
                     'categories@': {
                         controller: 'CategoriesListCtrl as categoriesListCtrl',
                         templateUrl: 'app/categories/categories.tmpl.html'
+                    },
+                    'bookmarks@': {
+                        controller: 'BookmarksListCtrl as bookmarksListCtrl',
+                        templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html'
                     }
                 }
             })
