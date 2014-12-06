fe8a79641fa3b47f8c7bcb05fc733b9bca69f4f8
Lukas Ruebbelke <simpul@gmail.com>
2014-12-06 13:15:37 -0700

    Fixing broken route

--- src/app/categories/categories.js
+++ src/app/categories/categories.js
@@ -1,7 +1,7 @@
 angular.module('categories', [
     'eggly.models.categories'
 ])
-    .config(function($stateProvider){
+    .config(function ($stateProvider) {
         $stateProvider
             .state('eggly.categories', {
                 url: '/',
@@ -9,12 +9,16 @@ angular.module('categories', [
                     'categories@': {
                         controller: 'CategoriesCtrl',
                         templateUrl: 'app/categories/categories.tmpl.html'
+                    },
+                    'bookmarks@': {
+                        controller: 'BookmarksListCtrl as bookmarksListCtrl',
+                        templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html'
                     }
                 }
             })
         ;
     })
-    .controller('CategoriesCtrl', function CategoriesCtrl($scope){
+    .controller('CategoriesCtrl', function CategoriesCtrl($scope) {
 
     })
 ;
