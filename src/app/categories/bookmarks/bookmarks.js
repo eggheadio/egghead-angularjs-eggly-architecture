9cd3ba1a5028d5bc869a8e391993abf4f422f40c
Lukas Ruebbelke <simpul@gmail.com>
2015-01-28 08:53:52 -0700

    Cleaning up

--- src/app/categories/bookmarks/bookmarks.js
+++ src/app/categories/bookmarks/bookmarks.js
@@ -29,10 +29,6 @@ angular.module('categories.bookmarks', [
                 bookmarksListCtrl.bookmarks = bookmarks;
             });
 
-        bookmarksListCtrl.isSelectedBookmark = function (bookmarkId) {
-            return $stateParams.bookmarkId == bookmarkId;
-        };
-
         bookmarksListCtrl.getCurrentCategory = CategoriesModel.getCurrentCategory;
         bookmarksListCtrl.getCurrentCategoryName = CategoriesModel.getCurrentCategoryName;
         bookmarksListCtrl.deleteBookmark = BookmarksModel.deleteBookmark;
