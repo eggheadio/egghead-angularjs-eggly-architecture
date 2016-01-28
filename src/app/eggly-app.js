b56aa27e221e66dfb70cc82deb09ba08c4765a55
Cree Provinsal <cree@provinsal.com>
2016-01-28 17:58:39 -0600

    Changed main.currentCategory to main.currentCategory.name

--- src/app/eggly-app.js
+++ src/app/eggly-app.js
@@ -71,7 +71,7 @@ angular.module('Eggly', [
             main.newBookmark = {
                 title: '',
                 url: '',
-                category: main.currentCategory
+                category: main.currentCategory.name
             };
         }
 
